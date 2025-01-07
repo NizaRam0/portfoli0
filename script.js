4104e1b36101414edefbd0ed00a1edd7889dfbea
NizaRam0 <nizaramadan07@gmail.com>
2025-01-07 12:24:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function openProj(){
     window.open('https://example.com','_blank');
 
 }
+
+ if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+    // This is Safari
+    console.log("Safari detected");
+    const meta = document.createElement("meta");
+    meta.name = "viewport";
+    meta.content = "width=device-width, initial-scale=1.0"; // Adjust if needed
+    document.head.appendChild(meta);
